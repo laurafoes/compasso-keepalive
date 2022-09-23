1842db2bfbbe9297a0ed9ac9889a98f00733e2e3
laufoes <foeslaura@gmail.com>
2022-09-23 20:40:03 -0300

    styles: update global styles e variaveis css

--- src/assets/styles/GlobalStyle.tsx
+++ src/assets/styles/GlobalStyle.tsx
@@ -2,12 +2,29 @@ import { createGlobalStyle } from "styled-components";
 import { normalize } from 'styled-normalize';
 
 export const GlobalStyle = createGlobalStyle`
+    ${normalize}
     * {
     box-sizing: border-box;
     font-family: 'Poppins', sans-serif;
+    font-weight: 400;
     font-size: 16px;
     margin: 0;
     padding: 0;
     text-decoration: none;
+    
+    h1 {
+        font-size: 60px;
+    }
+    h2 {
+        font-size: 64px;
+        font-weight: 700;
+    }
+    h3 {
+        font-size: 36px;
+        font-weight: 700;
+    }
+    h4 {
+        font-size: 30px; 
+    }
     }
 `
---
+++ src/assets/styles/variables.tsx
@@ -0,0 +1,11 @@
+//Textos
+export const corPrincipal = "#E0E0E0";
+export const corSecundaria = "#222222";
+export const corDestaque = "#C12D18";
+
+//Fundo
+export const fundoPrincipal = "linear-gradient(180deg, #33383D 0%, #1C1D20 100%)";
+export const fundoFooter = "linear-gradient(90.16deg, #33383D 0%, #1C1D20 100%)";
+
+//Botoes
+export const fundoBotaoPrincipal = " linear-gradient(90deg, #FF2D04 0%, #C13216 100%);"
