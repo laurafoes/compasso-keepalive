418bde9beaaee706097514c6e27f6bd988e9e7ba
laufoes <foeslaura@gmail.com>
2022-09-25 15:15:49 -0300

    style: text size and spacing fix

--- src/components/Missao/MissaoElements.tsx
+++ src/components/Missao/MissaoElements.tsx
@@ -6,17 +6,23 @@ export const Container = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: flex-end;
+    text-align: right;
+    width: 52.55vw;
 `
 
 export const Destaque = styled.h3`
     color: ${corDestaque};
     font-weight: 700;
+    line-height: 46px;
 `
 
 export const Traducao = styled.p`
     color: ${corSecundaria};
+    font-size: 24px;
+    line-height: 30px;
 `
 export const DestaqueGrande = styled.h2`
     color: ${corDestaque};
     font-weight: 700;
+    line-height: 81px;
 `
