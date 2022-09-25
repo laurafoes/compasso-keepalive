e29f783484c75228e346e4cf2cb9c4631bfd4469
laufoes <foeslaura@gmail.com>
2022-09-25 14:58:18 -0300

    feat: use states provider by Date and Time widget

--- src/components/DisplayData/index.tsx
+++ src/components/DisplayData/index.tsx
@@ -1,10 +1,34 @@
 import { Container, Data, Hora } from "./DisplayDataElements"
+import { useContext, useEffect } from 'react';
+import { WidgetDataContext } from "../../common/context/WidgetData";
+import { PropsWidgetDataContext, WidgetDataContextProps } from "../interfaces/WidgetData";
 
-export const DisplayData = () => {
+export const DisplayData = ({ children }: WidgetDataContextProps) => {
+    const { dataHora, setDataHora } = useContext<PropsWidgetDataContext>(WidgetDataContext);
+
+    function obtemDataEHora() {
+        const locale = 'pt-br';
+        const dataCompleta = new Date().toLocaleDateString(locale, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+        const horaCompleta = new Date().toLocaleTimeString(locale, { hour: '2-digit', minute: '2-digit' });
+
+        setDataHora({ data: dataCompleta, hora: horaCompleta })
+        console.log( dataCompleta);
+    }
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            obtemDataEHora();
+        }, 60 * 10);
+
+        return () => {
+            clearInterval(timer);
+        }
+    }, []);
+
     return(
         <Container>
-            <Hora>11:26</Hora>
-            <Data>terca-feira, 17 de março de 2020</Data>
+                <Hora>{ dataHora.hora } </Hora>
+                <Data>{ dataHora.data } </Data>
         </Container>
     )
 }
