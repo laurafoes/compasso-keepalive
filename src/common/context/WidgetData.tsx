a794edc2fbb5542e18c2ea6d92d5468130dc29d8
laufoes <foeslaura@gmail.com>
2022-09-25 15:30:02 -0300

    fix: erase unused code

--- src/common/context/WidgetData.tsx
+++ src/common/context/WidgetData.tsx
@@ -14,24 +14,6 @@ WidgetDataContext.displayName = 'Widget de Data / Hora';
 export const WidgetDataProvider = ({ children }: WidgetDataContextProps) => {
     const [dataHora, setDataHora] = useState(valorInicial.dataHora);
 
-    // const obtemDataEHora => () {
-    // const data = new Date();
-    // const diasDaSemana = new Array ('domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado');
-
-    // const dia = data.getDate();
-    // const diaDaSemana = diasDaSemana[data.getDay()];
-    // const mes = data.getMonth();
-    // const ano = data.getFullYear();
-    // const hora = data.getHours();
-    // const minuto = data.getMinutes();
-
-    // const string_hora = `${hora}:${minuto}`;
-    // const string_data = `${diaDaSemana}, ${dia} de ${mes} de ${ano}`
-
-    // return string_hora string_data
-
-
-
     return (
         <WidgetDataContext.Provider value={{ dataHora, setDataHora }}>
             {children}
