864e0154d2f1b79dbe625a65fc5d7ea5c67d3b77
laufoes <foeslaura@gmail.com>
2022-09-26 11:40:00 -0300

    feat: adds onClick interface

--- src/components/FooterBtn/index.tsx
+++ src/components/FooterBtn/index.tsx
@@ -2,7 +2,8 @@ import { Button } from "./FooterBtnElements"
 
 interface Props {
     variant: boolean,
-    children?: React.ReactNode
+    children?: React.ReactNode,
+    onClick?: React.MouseEvent
 }
 
 export const FooterBtn = ({ variant, children }: Props) => {
