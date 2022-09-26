d90a4c25e76b6f3d3c7eb5f8dbc38a8a4446aa3f
laufoes <foeslaura@gmail.com>
2022-09-26 11:37:44 -0300

    feat: manage states, implements validations

--- src/components/Inputs/InputPassword/InputPassword.tsx
+++ src/components/Inputs/InputPassword/InputPassword.tsx
@@ -1,14 +1,34 @@
-import { TiLockClosedOutline } from 'react-icons/ti';
-import { Container, Input, PasswordIcon } from '../InputElements';
+import { useContext } from 'react';
+import { UserInfoContext } from '../../../common/context/UserInfo';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { PropsUserContext } from '../../interfaces/UserInfo';
+import { Container, Input, MensagemErro, PasswordIcon } from '../InputElements';
 
 export const InputPassword = () => {
+    const { user, setUser, erro, existeErro, icone, setIcone } = useContext<PropsUserContext>(UserInfoContext);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({...user, [e.target.name]: e.target.value});
+        setIcone({...icone, [e.target.name]: true});
+    }
+
     return(
         <Container>
-            <Input type="text" placeholder="Senha">
+            <Input
+                type="password"
+                placeholder="Senha"
+                name="password"
+                className="password_size"
+                onChange={((e: any) => handleChange(e))}
+                existeErro={existeErro}
+            >
             </Input>
-            <PasswordIcon>
-                <TiLockClosedOutline />
+            <PasswordIcon icone={icone.password}>
+                <FontAwesomeIcon icon="fa-regular fa-lock-keyhole" />
             </PasswordIcon>
+            <MensagemErro>
+                {erro}
+            </MensagemErro>
         </Container>
     )
 }
