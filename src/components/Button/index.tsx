a33c35f1036265bfb9c724ed0f91508d23c8cd80
laufoes <foeslaura@gmail.com>
2022-09-26 11:29:45 -0300

    feat: implements validations and states management  on Login Form

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,33 @@
+import { UserInfoContext } from '../../common/context/UserInfo';
+import { PropsUserContext } from '../interfaces/UserInfo';
 import { Botao } from './ButtonElements';
+import { useContext, useEffect } from 'react';
+import { validar } from '../../helpers/loginHelper';
+import { useNavigate } from 'react-router-dom';
 
 export const Button = () => {
+    const { user, erro, setErro, existeErro, setExisteErro } = useContext<PropsUserContext>(UserInfoContext);
+    const navigateTo = useNavigate();
+
+    useEffect(() => {
+        if(erro === '') {
+            setExisteErro(false)
+        } else {
+            setExisteErro(true)
+        }
+        console.log(`${existeErro} detecta se existe erro`)
+    }, [erro])
+    
+    const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        setErro(validar(user));
+        if(erro) {
+            return navigateTo("/home");
+        }
+    }
+
     return(
-        <Botao>
+        <Botao onClick={handleSubmit} existeErro={existeErro}>
             Continuar
         </Botao>
     )
