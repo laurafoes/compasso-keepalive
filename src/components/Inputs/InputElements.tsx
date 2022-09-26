3c04c8eb4bdd1d0e5aa2fe4cb938f617d11629dc
laufoes <foeslaura@gmail.com>
2022-09-26 11:38:52 -0300

    feat: implements styling props

--- src/components/Inputs/InputElements.tsx
+++ src/components/Inputs/InputElements.tsx
@@ -1,35 +1,50 @@
 import styled from 'styled-components';
 import { AiOutlineUser } from 'react-icons/ai';
 import { TiLockClosedOutline } from 'react-icons/ti';
+import { corErro } from '../../assets/styles/variables';
+
+interface Props {
+    existeErro?: boolean,
+    icone?: boolean
+}
 
 export const Container = styled.div`
     width: 379px;
     position: relative;
     `
 
-export const Input = styled.input`
+export const Input = styled.input<Props>`
     width: 100%;
-    border: 1px solid white;
+    border-color: ${({existeErro}) => existeErro ? corErro : 'white' };
+    border-width: 1px;
     border-radius: 50px;
     background-color: transparent;
     display: flex;
     align-items: center;
     margin-bottom: 1.9rem;
     padding: 20px;
-        &::placeholder {
-            color: white;
+    &::placeholder {
+        color: white;
     }
 `
 
-export const UserIcon = styled(AiOutlineUser)`
+export const UserIcon = styled(AiOutlineUser)<Props>`
     position: absolute;
-    right: -2.25rem;
-    top: 1rem;
-    font-size: 24px;
+    transition: ease 300ms;
+    right: ${({icone}) => icone ? "2rem" : "-2.25rem" };
+    top: 0.75rem;
+    font-size: 26px;
 `
-export const PasswordIcon = styled(TiLockClosedOutline)`
+export const PasswordIcon = styled(TiLockClosedOutline)<Props>`
     position: absolute;
-    right: -2.25rem;
-    top: 1rem;
-    font-size: 24px;
+    transition: ease 300ms;
+    right: ${({icone}) => icone ? "2rem" : "-2.5rem" };
+    top: 0.75rem;
+    font-size: 32px;
+`
+export const MensagemErro = styled.p<Props>`
+    text-align: center;
+    color: ${corErro};
+    font-weight: 700;
+    line-height: 20.28px;
 `
