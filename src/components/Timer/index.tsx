335ab454478f4e1222c870b7365e76346d2a10a1
laufoes <foeslaura@gmail.com>
2022-09-26 11:19:55 -0300

    feat: redirects do login page when counter hits 0

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Container, Mensagem, Contador, TimerWrapper } from "./TimerElements"
 
 export const Timer = () => {
     const [ counter, setCounter ] = useState(60);
+    const navigateTo = useNavigate();
 
     useEffect(() => {
         counter > 1 && setTimeout(() => setCounter( counter - 1 ), 1000);
+        counter === 1 ? setTimeout(() => navigateTo("/"), 1000) : '';
     }, [counter]);
 
     return(
