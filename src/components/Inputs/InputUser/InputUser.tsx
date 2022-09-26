7809d2cb7be687658de4144953bec6be8dcf4068
laufoes <foeslaura@gmail.com>
2022-09-26 11:37:18 -0300

    feat: manage states, implements validations

--- src/components/Inputs/InputUser/InputUser.tsx
+++ src/components/Inputs/InputUser/InputUser.tsx
@@ -1,12 +1,28 @@
+import { useContext } from 'react';
 import { AiOutlineUser } from 'react-icons/ai';
+import { UserInfoContext } from '../../../common/context/UserInfo';
+import { PropsUserContext } from '../../interfaces/UserInfo';
 import { Container, Input, UserIcon } from '../InputElements';
 
 export const InputUser = () => {
+    const { user, setUser, existeErro, icone, setIcone } = useContext<PropsUserContext>(UserInfoContext);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({...user, [e.target.name]: e.target.value});
+        setIcone({...icone, [e.target.name]: true});
+    }
+
     return(
         <Container>
-            <Input type="text" placeholder="Usuário">
+            <Input
+                type="text"
+                placeholder="Usuário"
+                name="email"
+                onChange={((e: any) => handleChange(e))}
+                existeErro={existeErro}
+            >
             </Input>
-            <UserIcon>
+            <UserIcon icone={icone.email}>
                 <AiOutlineUser />
             </UserIcon>
         </Container>
