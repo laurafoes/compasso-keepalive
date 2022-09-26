d13ea8acde89554df1c8b8ac350824f49898305f
laufoes <foeslaura@gmail.com>
2022-09-26 11:23:41 -0300

    feat: create UserContext

---
+++ src/common/context/UserInfo.tsx
@@ -0,0 +1,34 @@
+import { createContext, useState } from 'react';
+import { PropsUserContext, UserContextProps } from '../../components/interfaces/UserInfo';
+
+const valorInicial = {
+    user: {
+        email: '',
+        password: ''
+    }, setUser: () => {},
+    existeErro: false,
+    setExisteErro: () => {},
+    erro: '',
+    setErro: () => {},
+    icone: {
+        email: false,
+        password: false,
+    },
+    setIcone: (e: any) => {}
+}
+
+export const UserInfoContext = createContext<PropsUserContext>(valorInicial);
+UserInfoContext.displayName = 'User Info';
+
+export const UserInfoProvider = ({ children }: UserContextProps) => {
+    const [ user , setUser ] = useState(valorInicial.user);
+    const [ existeErro, setExisteErro ] = useState(false);
+    const [ erro, setErro ] = useState(valorInicial.erro);
+    const [ icone, setIcone ] = useState(valorInicial.icone);
+
+    return (
+        <UserInfoContext.Provider value={{ user, setUser, existeErro, setExisteErro, erro, setErro, icone, setIcone }}>
+            {children}
+        </UserInfoContext.Provider>
+    )
+}
