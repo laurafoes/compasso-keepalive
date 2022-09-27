a9d33304c27369d0993c46a0b70c44b68f55c475
laufoes <foeslaura@gmail.com>
2022-09-27 08:39:50 -0300

    feat: implements weather forecast based on user location

--- src/components/Clima/index.tsx
+++ src/components/Clima/index.tsx
@@ -1,16 +1,36 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { TiWeatherPartlySunny } from 'react-icons/ti';
 import { Cidade, ClimaWrapper, Container, Icone, Temperatura } from './ClimaElements';
 
 export const Clima = () => {
+    const [ temperatura, setTemperatura ] = useState<number>(0);
+    const [ cidade, setCidade ] = useState<string>('');
+     
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(async function (position) {
+            let latitude = position.coords.latitude;
+            let longidute = position.coords.longitude;
+
+            axios.get(`https://api.openweathermap.org/data/2.5/weather/?lat=${latitude}&lon=${longidute}&units=metric&APPID=e75376c3fdfbe9b424e6194dc179030c`)
+                .then(res => res.data)
+                .then(res => {
+                    const { main, name, id } = res;
+                    setCidade(`${name} - SC`);
+                    setTemperatura(Math.round(main.temp));
+                });
+        });
+    }, []);
+
     return(
         <Container>
-            <Cidade>Passo Fundo - RS</Cidade>
+            <Cidade>{cidade}</Cidade>
             <ClimaWrapper>
                 <Icone>
                     <TiWeatherPartlySunny />
                 </Icone>
                 <Temperatura>
-                    22°
+                    {temperatura}
                 </Temperatura>
             </ClimaWrapper>
         </Container>
