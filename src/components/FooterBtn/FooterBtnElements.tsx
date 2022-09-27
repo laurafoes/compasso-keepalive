074cc6824fbf9bff411ec2cc8d076b79d0d0fd6b
laufoes <foeslaura@gmail.com>
2022-09-26 23:32:32 -0300

    styles: minor fix

--- src/components/FooterBtn/FooterBtnElements.tsx
+++ src/components/FooterBtn/FooterBtnElements.tsx
@@ -9,7 +9,8 @@ export const Button = styled.button<Props>`
     height: 9.26vh;
     width: 50%;
     font-size: 12px;
-    margin-top: 0px;
+    margin-top: -0.45rem;
+    padding: 0.75rem 0.25rem 0 0.25rem;
     border: none;
     cursor: pointer;
     color: ${({variant}) => variant ? corDestaque : corPrincipal };
