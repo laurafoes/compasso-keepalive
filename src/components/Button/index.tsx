7fa21dc0c738d2779aa0822f3cfd5262cb6e9476
laufoes <foeslaura@gmail.com>
2022-09-27 10:07:16 -0300

    fix: login validation

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,28 +1,24 @@
 import { UserInfoContext } from '../../common/context/UserInfo';
 import { PropsUserContext } from '../interfaces/UserInfo';
 import { Botao } from './ButtonElements';
-import { useContext, useEffect } from 'react';
-import { validar } from '../../helpers/loginHelper';
+import { useContext } from 'react';
+import { regexEmail } from '../../helpers/loginHelper';
 import { useNavigate } from 'react-router-dom';
 
 export const Button = () => {
-    const { user, erro, setErro, existeErro, setExisteErro } = useContext<PropsUserContext>(UserInfoContext);
+    const { user, setErro, existeErro, setExisteErro } = useContext<PropsUserContext>(UserInfoContext);
     const navigateTo = useNavigate();
-
-    useEffect(() => {
-        if(erro === '') {
-            setExisteErro(false)
-        } else {
-            setExisteErro(true)
-        }
-        console.log(`${existeErro} detecta se existe erro`)
-    }, [erro])
     
     const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        setErro(validar(user));
-        if(erro) {
-            return navigateTo("/home");
+
+        if (regexEmail.test(user.email) && user.password.length > 5) {
+            setErro('');
+            setExisteErro(false);
+            navigateTo('/home');
+        } else {
+            setErro('Ops, usuário ou senha inválidos. Tente novamente!');
+            setExisteErro(true);
         }
     }
 
