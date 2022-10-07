bf8bbeabb20881be7c5a9248be184b6e712abeac
laufoes <foeslaura@gmail.com>
2022-10-07 14:22:54 -0300

    feat: adds routes to Register page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,18 @@
-import { Login } from "./pages/Login";
+import { PaginaInicial } from "./pages/PaginaInicial";
 import { Home } from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
 
 export default function Router() {
     return(
         <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
+            <Route path='/' element={<PaginaInicial />}>
+                <Route index element={<Login />} />
+                <Route path='/register' element={<Register />} />
+            </Route>
+            <Route path='/home' element={<Home />} />
         </Routes>
       </BrowserRouter>
     )
